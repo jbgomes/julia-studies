d035f8fe6bba0777f48876144b5b1385552d081f
julia <julia.gomes@ee.ufcg.edu.br>
2022-06-17 14:52:14 -0300

    [DIV] Added function that divides complex numbers

--- complex-numbers/complex-numbers.jl
+++ complex-numbers/complex-numbers.jl
@@ -1,4 +1,4 @@
-import Base: *, +, -, abs, conj, real, imag
+import Base: *, +, -, abs, conj, real, imag, /, ==, ≈
 
 struct ComplexNumber <: Number
     real::Number
@@ -9,11 +9,14 @@ end
 *(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real * b. real - a.imag * b.imag, a.imag * b.real + a.real * b.imag)
 +(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real + b.real, a.imag + b.imag)
 -(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real - b.real, a.imag - b.imag)
+/(a::ComplexNumber, b::ComplexNumber) = ComplexNumber((a.real * b.real + a.imag * b.imag)/abs(b)^2 , (a.imag * b.real - a.real * b.imag)/abs(b)^2)
 
 abs(a::ComplexNumber) = (a.real^2 + a.imag^2)^0.5
 conj(a::ComplexNumber) = ComplexNumber(a.real, - a.imag)
 real(a::ComplexNumber) = a.real
 imag(a::ComplexNumber) = a.imag
 
-#/(a::ComplexNumber, b::ComplexNumber) = ComplexNumber((a.real * b.real + a.imag * b.imag)/abs(b)^2 , (a.imag * b.real - a.real * b.imag)/abs(b)^2)
+==(a::ComplexNumber, b::ComplexNumber) = isapprox(a.real, b.real) && isapprox(a.imag, b.imag)
+≈(a::ComplexNumber, b::ComplexNumber) = isapprox(a.real, b.real) && isapprox(a.imag, b.imag)
+
 #(a::ComplexNumber, b::ComplexNumber) = ComplexNumber(a.real = b.real) && (a.imag = b.imag)
